b27f39a8c32a5622295e29422f2e47fd65e3a010
Jason Wang <jinqu7777@163.com>
2019-03-18 23:05:05 +0800

    add one more method comma is needed between method

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ new Vue({
     methods: {
         greet: function(time){
             return 'Good ' + time + ', ' + this.name;
+        },
+        //between method comma is mandatory
+        fight: function (date) {
+            return 'fight' + date  + this.job;
+
         }
     }
 });
